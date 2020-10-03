c9958d38285450386c6204ae7b768fb432471d5a
wangpeng <867007845@qq.com>
2020-10-03 21:54:52 +0800

    Initial commit

---
+++ src/main/java/driftmen/live/demojetkins/DemoJetkinsApplication.java
@@ -0,0 +1,13 @@
+package driftmen.live.demojetkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJetkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoJetkinsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/driftmen/live/demojetkins/DemoJetkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package driftmen.live.demojetkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJetkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
